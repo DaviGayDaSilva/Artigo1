9bdf4da479e70d4ac1257b0f6637ed34685361af
DaviGayDaSilva <languith54@gmail.com>
2025-08-31 12:17:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`🚀 Bot online! Nome: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === '!ping') {
+    message.channel.send('Pong! 🏓');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
